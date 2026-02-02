afd9e50efade46f059fefa7dfd22ab4ffc703dfc
parvdabhi1234-sudo <parvdabhi1234@gmail.com>
2026-02-02 22:15:10 +0530

    Variables practice

---
+++ variables.py
@@ -0,0 +1,17 @@
+# Varibles practice
+name = "Parv"
+age = 19
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Extra practice
+x = 10
+y = 3
+
+print("Addition:", x+y)
+print("Type of age:", type(age))
