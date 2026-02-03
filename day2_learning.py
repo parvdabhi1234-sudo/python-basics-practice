f3e9d5a1d7128ed61d3d4b722e482677bf4b333a
parvdabhi1234-sudo <parvdabhi1234@gmail.com>
2026-02-03 16:23:51 +0530

    Day 2: Learning Operators and If-Else

---
+++ day2_learning.py
@@ -0,0 +1,39 @@
+# Operators example 
+
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+# Comparison operators
+
+x = 5
+y = 10
+
+print(x > y)
+print(x < y)
+print(x == y)
+
+# If-Else examples
+
+age = 20
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+# If-Elif-Else
+
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+else:
+    print("Grade C")
