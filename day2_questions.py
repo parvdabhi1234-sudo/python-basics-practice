d08ab9b9b2d420b499df90b1add3acebafc4aabd
parvdabhi1234-sudo <parvdabhi1234@gmail.com>
2026-02-03 16:35:29 +0530

    Day 2: Practice questions

---
+++ day2_questions.py
@@ -0,0 +1,28 @@
+# Practice Questions
+
+# Q1: Check even or odd
+
+num = 7
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Q2: Largest of two numbers
+
+a = 15
+b = 20
+
+if a > b:
+    print("A is greater")
+else:
+    print("B is greater")
+
+# Q3: Check voting eligbility
+
+age = 17
+
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not eligible")
